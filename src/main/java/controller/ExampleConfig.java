4a43f84fee4a483c7af4a83cb4e0c3239e8387b9
kunal0721 <kunalbamoriya984@gmail.com>
2026-05-27 11:59:19 +0530

    servlet context work

---
+++ src/main/java/controller/ExampleConfig.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ExampleConfig extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public ExampleConfig() {
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		ServletConfig config = getServletConfig();
+	
+		String ans = config.getInitParameter("work");
+		response.getWriter().println(ans);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+}
